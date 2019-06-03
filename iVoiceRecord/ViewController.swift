ce6f7f87b2fb5f29aec51f69fa8c10a31b7fc912
Vladyslav Tkach <vladyslav.tkach@sannacode.com>
2019-06-03 21:59:52 +0300

    Initial Commit

---
+++ iVoiceRecord/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iVoiceRecord
+//
+//  Created by Vlad Tkach on 6/3/19.
+//  Copyright © 2019 Vlad Tkach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
